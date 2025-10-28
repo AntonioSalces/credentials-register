ae1146a09c685b8213f2a241360059de4466bfec
Antonio Salces Alcaraz <antoniosalces06@gmail.com>
2025-10-28 22:34:54 +0100

    componente register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -17,6 +17,26 @@ export class RegisterComponent {
       password: ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)]]    
     });
   }
+
+  getError(controlName: string): string {
+  const control = this.registerForm.get(controlName);
+  if (control?.errors) {
+    if (control.errors['required']) {
+      return 'Este campo es obligatorio';
+    }
+    if (control.errors['email']) {
+      return 'Introduce un email válido';
+    }
+    if (control.errors['pattern']) {
+      return 'La contraseña debe tener mayúsculas, minúsculas, números y al menos 8 caracteres';
+    }
+    if (control.errors['minlength']) {
+      return 'La contraseña es demasiado corta';
+    }
+  }
+  return '';
+}
+
 
   onSubmit() {
     if (this.registerForm.valid) {
